26f1d3cec977cc8f75d5ab0e2cd4b17eea104874
AndreGomesSilva <silva_andregomes@outlook.com>
2024-07-10 10:40:17 -0300

    add: option to user choose between the contacts in SEARCH function

--- module00/ex01/phoneBook.cpp
+++ module00/ex01/phoneBook.cpp
@@ -11,9 +11,12 @@
 /* ************************************************************************** */
 
 #include "phoneBook.hpp"
+#include "contact.hpp"
 #include <iomanip>
 #include <ios>
 #include <iostream>
+#include <string>
+#include <cstdlib>
 
 Phonebook::Phonebook(void)
 {
@@ -76,12 +79,61 @@ static void	displayTableHeader(void)
 				<< " | " << std::endl;
 }
 
+static void	displayAllTableHeader(void)
+{
+	std::cout << std::right << std::setw(10) << "Index"
+				<< " | ";
+	std::cout << std::right << std::setw(10) << "First Name"
+				<< " | ";
+	std::cout << std::right << std::setw(10) << "Last Name"
+				<< " | ";
+	std::cout << std::right << std::setw(10) << "Nick Name"
+				<< " | " ;
+	std::cout << std::right << std::setw(10) << "Phone Number"
+				<< " | ";
+	std::cout << std::right << std::setw(10) << "Darkest Secret"
+	           << " | " << std::endl;
+}
+
 static void	displayContact(Contact contact, int index)
 {
 	std::cout << std::right << std::setw(10) << index + 1 << " | ";
 	std::cout << std::right << std::setw(10) << contact.getFirstName() << " | ";
 	std::cout << std::right << std::setw(10) << contact.getLastName() << " | ";
 	std::cout << std::right << std::setw(10) << contact.getNickName() << " | " << std::endl;
+}
+
+static void displayAllContentOfContact(const Contact& contact, int index)
+{
+    std::cout << std::right << std::setw(10) << index + 1 << " | ";
+	std::cout << std::right << std::setw(10) << contact.getFirstName() << " | ";
+	std::cout << std::right << std::setw(10) << contact.getLastName() << " | ";
+	std::cout << std::right << std::setw(10) << contact.getNickName() << " | ";
+	std::cout << std::right << std::setw(10) << contact.getPhoneNumber() << " | ";
+	std::cout << std::right << std::setw(10) << contact.getDarkSecret() << " | " << std::endl;
+}
+
+static void chooseContact(const Contact *contacts)
+{
+    std::string  input;
+    
+    input  = getInput("Type the ID of the contact that do you want to see or Q to leave: ");
+    if (input.length() == 1 && (input[0] == 'Q' || input[0]  == 'q'))
+        return ;
+    if (input.length() ==  1  && input[0] >= '1' && input[0] <= '8')
+    {
+        int index;
+        index = atoi(input.c_str())  -  1;
+        if (contacts[index].getFirstName().empty())
+            std::cout << "This Contact does not exist" << std::endl;
+        else {
+            displayAllTableHeader();
+            displayAllContentOfContact(contacts[index], index);
+        }
+    }
+    else {
+        std::cout << "Please type a valid ID" << std::endl;
+    }
 }
 
 void Phonebook::searchContact(void) const
@@ -99,4 +151,5 @@ void Phonebook::searchContact(void) const
       break;
 		displayContact(this->_contacts[i], i);
 	}
+	chooseContact(this->_contacts);
 }
