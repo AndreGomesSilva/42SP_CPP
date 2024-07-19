1f5e2b316aa7b8c5375662079058415b68a11c95
AndreGomesSilva <silva_andregomes@outlook.com>
2024-07-19 10:32:50 -0300

    module 01 ex03

---
+++ module01/ex03/Weapon.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include "Weapon.hpp"
+
+Weapon::Weapon()
+{
+}
+
+Weapon::Weapon(std::string type) : _type(type)
+{
+}
+
+Weapon::~Weapon()
+{
+}
+
+std::string const &Weapon::getType() const
+{
+  return (_type);
+}
+
+void    Weapon::setType(std::string type)
+{
+  _type = type;
+}
+
+
+
