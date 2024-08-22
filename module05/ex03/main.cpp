e0bc9e9fdfc20221d6b3713983ccda01a14df5cc
AndreGomesSilva <silva_andregomes@outlook.com>
2024-08-22 11:14:30 -0300

    fix: solve leak

--- module05/ex03/main.cpp
+++ module05/ex03/main.cpp
@@ -1,22 +1,24 @@
-#include "Intern.hpp"
 #include "AForm.hpp"
+#include "Intern.hpp"
 #include <ostream>
 
 int main() {
 
   std::cout << std::endl;
-  std::cout << "                  ______EX03______                      " << std::endl;
+  std::cout << "                  ______EX03______                      "
+            << std::endl;
   std::cout << std::endl;
   std::cout << "-------Constructors-------" << std::endl;
   Intern someRandomIntern;
-  AForm *rrf;
+  AForm *rrf = NULL;
   std::cout << std::endl;
 
   std::cout << "-------Functions-------" << std::endl;
-  someRandomIntern.makeForm("robotomy request", "Bender");
+  rrf = someRandomIntern.makeForm("robotomy request", "Bender");
   std::cout << std::endl;
-  
-  std::cout << rrf << std::endl;
+
+  std::cout << *rrf << std::endl;
   std::cout << "-------Destructors-------" << std::endl;
+  delete rrf;
   return (0);
 }
