de67bc5ef335b5714dd1568c2b0f51ed2fb274a5
AndreGomesSilva <silva_andregomes@outlook.com>
2024-08-30 19:57:14 -0300

    fix: Mutant

--- module08/ex02/MutantStack.hpp
+++ module08/ex02/MutantStack.hpp
@@ -10,6 +10,9 @@ class MutantStack : public std::stack<T>
   public:
     typedef typename std::stack<T>::container_type::iterator iterator;
     typedef typename std::stack<T>::container_type::const_iterator const_iterator;
+    typedef typename std::stack<T>::container_type::reverse_iterator reverse_iterator;
+    typedef typename std::stack<T>::container_type::const_reverse_iterator const_reverse_iterator;  
+
     MutantStack();
     MutantStack(const MutantStack &obj);
     MutantStack &operator=(const MutantStack &obj);
