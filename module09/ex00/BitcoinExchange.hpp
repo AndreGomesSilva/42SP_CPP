304aeebdf295d0f3dd7510fb6721f0409e5b4323
AndreGomesSilva <silva_andregomes@outlook.com>
2024-09-02 12:07:58 -0300

    WIP btc class

--- module09/ex00/BitcoinExchange.hpp
+++ module09/ex00/BitcoinExchange.hpp
@@ -2,7 +2,19 @@
 #define BITCOINEXCHANGE_HPP
 
 #include "iostream"
+#include "map"
+#include <map>
 
+class Btc {
+ private:
+    std::map<std::string, float> db;
+
+ public:
+    Btc();
+    Btc(const Btc &other);
+    ~Btc();
+
+};
 
 
 #endif
--- module09/ex00/main.cpp
+++ module09/ex00/main.cpp
@@ -1,20 +1,37 @@
 #include <fstream>
 #include <iostream>
+#include <map>
+#include <string>
+
+int getFileContent(std::string file, std::map<std::string, float> &db)
+{
+
+}
+
+int validateFile(std::string input) {
+  if (input.empty())
+    return 1;
+  std::string extension = input.substr(input.find(".") + 1);
+  if (extension != "txt" || extension != "csv")
+    return 1;
+  std::ifstream file(input);
+  if (!file) {
+    std::cerr << "Error: Could not open file " << input << std::endl;
+    return 1;
+  }
+  return 0;
+}
 
 int main(int argc, char **argv) {
-  if (argc != 2)
+  if (argc != 2) {
+    std::cerr << "Usage:  " << argv[0] << " <filename>" << std::endl;
+    return 1;
+  }
+  std::string input = argv[1];
+  if (!validateFile(input))
   {
-      std::cerr << "Usage:  " << argv[0]  << " <filename>" << std::endl;
-      return 1;
+    std::cerr << "Invalid File" << std::endl;
+    return 1;
   }
-
-  std::ifstream file(argv[1]);
-  if (!file) {
-      std::cerr << "Error: Could not open file " << argv[1] << std::endl;
-      return 1;
-  }
-
-  std::cout << "------------" << std::endl;
-  std::cout << std::endl;
   return 0;
 }
